ad50ae011a4fb25c5c16d07238b03354c83fa96e
Harsh-Sinha <harsh.sinha@hotmail.com>
2023-11-18 16:06:01 -0500

    use io_uring for write op

--- glibc/src/sysdeps/unix/sysv/linux/write.c
+++ glibc/src/sysdeps/unix/sysv/linux/write.c
@@ -19,11 +19,32 @@
 #include <unistd.h>
 #include <sysdep-cancel.h>
 
+#include "io_uring_backed_io.h"
+
 /* Write NBYTES of BUF to FD.  Return the number written, or -1.  */
 ssize_t
 __libc_write (int fd, const void *buf, size_t nbytes)
 {
-  return SYSCALL_CANCEL (write, fd, buf, nbytes);
+  ssize_t ret = nbytes;
+
+  // emplace request
+  struct iovec request;
+  request.iov_base = buf;
+  request.iov_len  = nbytes;
+  struct io_uring_sqe* sqe = io_uring_get_sqe(&g_io_uring);
+  io_uring_prep_writev(sqe, fd, &request, 1, 0);
+  io_uring_submit(&g_io_uring);
+
+  // wait for completion
+  struct io_uring_cqe *cqe;
+  io_uring_wait_cqe(&g_io_uring, &cqe);
+  if (cqe->res < 0)
+  {
+    ret = cqe->res;
+  }
+  io_uring_cqe_seen(&g_io_uring, cqe);
+
+  return ret;
 }
 libc_hidden_def (__libc_write)
 
