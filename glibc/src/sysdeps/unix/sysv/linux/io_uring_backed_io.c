d3ea5f414c1f4fccae4fab336cea86ce807603f0
Harsh-Sinha <harsh.sinha@hotmail.com>
2023-11-26 12:01:34 -0500

    fix definition of global io_uring

---
+++ glibc/src/sysdeps/unix/sysv/linux/io_uring_backed_io.c
@@ -0,0 +1,36 @@
+#include "io_uring_backed_io.h"
+
+#include <stdio.h>
+
+static int g_constructor_has_run = 0;
+
+struct io_uring g_io_uring;
+
+void io_uring_infra_init_impl(void)
+{
+    if (g_constructor_has_run == 0)
+    {
+        int flags = IORING_SETUP_SQPOLL;
+        int ring_fd = io_uring_queue_init(8, &g_io_uring, flags);
+        if (ring_fd < 0)
+        {
+            printf("failure to init io_uring\n");
+        }
+        else
+        {
+            printf("init success!\n");
+        }
+        g_constructor_has_run = 1;
+    }
+
+}
+
+void io_uring_infra_deinit_impl(void)
+{
+    if (g_constructor_has_run == 1)
+    {
+        io_uring_queue_exit(&g_io_uring);
+        printf("deinit success!\n");
+        g_constructor_has_run = 0;
+    }
+}
--- glibc/src/sysdeps/unix/sysv/linux/io_uring_backed_io.h
+++ glibc/src/sysdeps/unix/sysv/linux/io_uring_backed_io.h
@@ -5,37 +5,20 @@
 
 #include "liburing.h"
 
-static int g_constructor_has_run = 0;
+extern struct io_uring g_io_uring;
 
-struct io_uring g_io_uring;
+void io_uring_infra_init_impl(void);
+
+void io_uring_infra_deinit_impl(void);
 
 static void __attribute__((constructor)) io_uring_infra_init(void)
 {
-    if (g_constructor_has_run == 0)
-    {
-        int flags = IORING_SETUP_SQPOLL;
-        int ring_fd = io_uring_queue_init(8, &g_io_uring, flags);
-        if (ring_fd < 0)
-        {
-            printf("failure to init io_uring\n");
-        }
-        else
-        {
-            printf("init success!\n");
-        }
-        g_constructor_has_run = 1;
-    }
-
+    io_uring_infra_init_impl();
 }
 
 static void __attribute__((destructor)) io_uring_infra_deinit(void)
 {
-    if (g_constructor_has_run == 1)
-    {
-        io_uring_queue_exit(&g_io_uring);
-        printf("deinit success!\n");
-        g_constructor_has_run = 0;
-    }
+    io_uring_infra_deinit_impl();
 }
 
 #endif // IO_URING_BACKED_IO
