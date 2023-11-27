cf6b8c292d155668336e041d4d704e9bfe52d76b
Harsh-Sinha <harsh.sinha@hotmail.com>
2023-11-27 17:48:43 -0500

    refactor to non-static constructor/desctructor attribute

--- glibc/src/sysdeps/unix/sysv/linux/io_uring_backed_io.c
+++ glibc/src/sysdeps/unix/sysv/linux/io_uring_backed_io.c
@@ -2,35 +2,24 @@
 
 #include <stdio.h>
 
-static int g_constructor_has_run = 0;
-
 struct io_uring g_io_uring;
 
-void io_uring_infra_init_impl(void)
+void io_uring_infra_init(void)
 {
-    if (g_constructor_has_run == 0)
+    int flags = IORING_SETUP_SQPOLL;
+    int ring_fd = io_uring_queue_init(8, &g_io_uring, flags);
+    if (ring_fd < 0)
     {
-        int flags = IORING_SETUP_SQPOLL;
-        int ring_fd = io_uring_queue_init(8, &g_io_uring, flags);
-        if (ring_fd < 0)
-        {
-            printf("failure to init io_uring\n");
-        }
-        else
-        {
-            printf("init success!\n");
-        }
-        g_constructor_has_run = 1;
+        printf("failure to init io_uring\n");
     }
-
+    else
+    {
+        printf("init success!\n");
+    }
 }
 
-void io_uring_infra_deinit_impl(void)
+void io_uring_infra_deinit(void)
 {
-    if (g_constructor_has_run == 1)
-    {
-        io_uring_queue_exit(&g_io_uring);
-        printf("deinit success!\n");
-        g_constructor_has_run = 0;
-    }
+    io_uring_queue_exit(&g_io_uring);
+    printf("deinit success!\n");
 }
--- glibc/src/sysdeps/unix/sysv/linux/io_uring_backed_io.h
+++ glibc/src/sysdeps/unix/sysv/linux/io_uring_backed_io.h
@@ -7,18 +7,8 @@
 
 extern struct io_uring g_io_uring;
 
-void io_uring_infra_init_impl(void);
+void __attribute__((constructor)) io_uring_infra_init(void);
 
-void io_uring_infra_deinit_impl(void);
-
-static void __attribute__((constructor)) io_uring_infra_init(void)
-{
-    io_uring_infra_init_impl();
-}
-
-static void __attribute__((destructor)) io_uring_infra_deinit(void)
-{
-    io_uring_infra_deinit_impl();
-}
+void __attribute__((destructor)) io_uring_infra_deinit(void);
 
 #endif // IO_URING_BACKED_IO
