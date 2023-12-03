b304d52755a8b2b00146a287adad84a5ecde1b64
harsh <harsh@eecs582.myguest.virtualbox.org>
2023-12-03 10:43:21 -0500

    add all syscalls

--- test-monkey/read-write.c
+++ test-monkey/read-write.c
@@ -1,6 +1,10 @@
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdarg.h>
 #include <liburing.h>
+
+# define __CHECK_OPEN_NEEDS_MODE(oflag) \
+  (((oflag) & O_CREAT) != 0 || ((oflag) & __O_TMPFILE) == __O_TMPFILE)
 
 static int g_io_uring_initialized;
 struct io_uring g_io_uring;
@@ -28,6 +32,35 @@ void io_uring_infra_deinit(void)
 {
     io_uring_queue_exit(&g_io_uring);
     printf("deinit success!\n");
+}
+
+/* Open FILE with access OFLAG.  If O_CREAT or O_TMPFILE is in OFLAG,
+   a third argument is the file protection.  */
+int
+open64 (const char *file, int oflag, ...)
+{
+  int mode = 0;
+
+  if (__CHECK_OPEN_NEEDS_MODE (oflag))
+    {
+      va_list arg;
+      va_start (arg, oflag);
+      mode = va_arg (arg, int);
+      va_end (arg);
+    }
+
+  // emplace request
+  struct io_uring_sqe* sqe = io_uring_get_sqe(&g_io_uring);
+  io_uring_prep_openat(sqe, AT_FDCWD, file, oflag | O_LARGEFILE, mode);
+  io_uring_submit(&g_io_uring);
+
+  // wait for completion
+  struct io_uring_cqe *cqe;
+  io_uring_wait_cqe(&g_io_uring, &cqe);
+  int ret = cqe->res;
+  io_uring_cqe_seen(&g_io_uring, cqe);
+
+  return ret;
 }
 
 /* Read NBYTES into BUF from FD.  Return the number read or -1.  */
@@ -89,6 +122,43 @@ write (int fd, const void *buf, size_t nbytes)
   //memset(buf2, 0, ret + 1);
   //memcpy(buf2, buf, ret);
   //printf("[straceish]: write {fd = %d, buf = [%s]} = %ld\n", fd, buf2, ret);
+
+  return ret;
+}
+
+/* Make all changes done to FD actually appear on disk.  */
+int
+fsync (int fd)
+{
+  // emplace request
+  struct io_uring_sqe* sqe = io_uring_get_sqe(&g_io_uring);
+  io_uring_prep_fsync(sqe, fd, 0);
+  io_uring_submit(&g_io_uring);
+
+  // wait for completion
+  struct io_uring_cqe *cqe;
+  io_uring_wait_cqe(&g_io_uring, &cqe);
+  int ret = cqe->res;
+  io_uring_cqe_seen(&g_io_uring, cqe);
+
+  return ret;
+}
+
+
+/* Close the file descriptor FD.  */
+int
+close (int fd)
+{
+  // emplace request
+  struct io_uring_sqe* sqe = io_uring_get_sqe(&g_io_uring);
+  io_uring_prep_close(sqe, fd);
+  io_uring_submit(&g_io_uring);
+
+  // wait for completion
+  struct io_uring_cqe *cqe;
+  io_uring_wait_cqe(&g_io_uring, &cqe);
+  int ret = cqe->res;
+  io_uring_cqe_seen(&g_io_uring, cqe);
 
   return ret;
 }
