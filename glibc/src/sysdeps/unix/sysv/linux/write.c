03151696e7ca1f6876684d8d962bec36d5781b68
Harsh-Sinha <harsh.sinha@hotmail.com>
2023-12-02 23:22:21 -0500

    use -1 offset for write

--- glibc/src/sysdeps/unix/sysv/linux/write.c
+++ glibc/src/sysdeps/unix/sysv/linux/write.c
@@ -32,7 +32,7 @@ __libc_write (int fd, const void *buf, size_t nbytes)
   request.iov_base = (void*)buf;
   request.iov_len  = nbytes;
   struct io_uring_sqe* sqe = io_uring_get_sqe(&g_io_uring);
-  io_uring_prep_writev(sqe, fd, &request, 1, 0);
+  io_uring_prep_writev(sqe, fd, &request, 1, -1);
   io_uring_submit(&g_io_uring);
 
   // wait for completion
