fe8373a0dfd9ee9bffa04a697f9215da9444f554
harsh <harsh@eecs582.myguest.virtualbox.org>
2023-12-09 20:14:14 -0500

    more complete sample program that makes all syscalls

--- sample_program/main.c
+++ sample_program/main.c
@@ -2,15 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
-    for (int i = 0; i < 1<<22; ++i)
-    {
-        int fd = open("sample.txt", O_RDONLY);
-	close(fd);
-    }
+    pid_t pid = getpid();
+
+    int fd = open("a_sample.txt", O_RDWR | O_CREAT);
+    
+    const char* text = "hello, world\n";
+    write(fd, text, strlen(text));
+    
+    fsync(fd);
+    
+    lseek(fd, 0, SEEK_SET); 
+    
+    char buffer[100] = { 0 };
+    read(fd, buffer, sizeof(buffer) - 1);
+
+    printf("Read from file: %s\n", buffer);
+    
+    close(fd);
 
     return 0;
 }
