2eaaa72976aded987b4ba0b16d3100c265d877c0
harsh <harsh@eecs582.myguest.virtualbox.org>
2023-12-09 20:18:12 -0500

    rename file that gets created

--- sample_program/main.c
+++ sample_program/main.c
@@ -8,20 +8,20 @@ int main(int argc, char* argv[])
 {
     pid_t pid = getpid();
 
-    int fd = open("a_sample.txt", O_RDWR | O_CREAT);
-    
+    int fd = open("sample.txt", O_RDWR | O_CREAT);
+
     const char* text = "hello, world\n";
     write(fd, text, strlen(text));
-    
+
     fsync(fd);
-    
+
     lseek(fd, 0, SEEK_SET); 
-    
+
     char buffer[100] = { 0 };
     read(fd, buffer, sizeof(buffer) - 1);
 
     printf("Read from file: %s\n", buffer);
-    
+
     close(fd);
 
     return 0;
